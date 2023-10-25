52cc770afa71f120029a86b1aa294a8887b75dd8
MvedmanPro <mailkgpage@gmail.com>
2023-10-24 20:05:15 -0400

    w07_final_project_weather_app

---
+++ final_project/domManipulation.js
@@ -0,0 +1,33 @@
+import { weatherData, getWeatherData } from '/cse121b/final_project/fetchData.js';
+
+let now = new Date();
+let dateTime = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+
+// Function to display weather data
+export function displayWeatherData() {
+    const weatherDiv = document.getElementById('weather');
+    if (weatherDiv) { // Check if the element exists
+        weatherDiv.innerHTML = ''; // Clear the div
+
+        // Use of template literals and array method (forEach)
+        weatherData.forEach(data => {
+            const weatherInfo = `
+                <h2>Weather in ${data.name}</h2>
+                <p>Right now: ${dateTime}</p> 
+                <p>${data.weather[0].description}</p>
+                <p>Temperature: ${(data.main.temp - 273.15).toFixed(1)}°C</p>
+                <p>Feels Like: ${(data.main.feels_like - 273.15).toFixed(1)}°C</p>
+                <p>Highest Temperature: ${(data.main.temp_max - 273.15).toFixed(1)}°C</p>
+                <p>Lowest Temperature: ${(data.main.temp_min - 273.15).toFixed(1)}°C</p>
+                <p>Wind: ${(data.wind.speed)}</p>`;
+            weatherDiv.innerHTML += weatherInfo;
+        });
+    }
+}
+
+// Event listener for the form submission
+document.getElementById('weather-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const city = document.getElementById('city').value;
+    getWeatherData(city).then(() => displayWeatherData());
+});
---
+++ final_project/fetchData.js
@@ -0,0 +1,12 @@
+// Importing the fetch module
+
+// Array to store fetched data
+export let weatherData = [];
+
+// Function to fetch weather data
+export async function getWeatherData(city) {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b409ee506ffe2f7f43e8a343d4018ab0`);
+    const data = await response.json();
+    console.log(data)
+    weatherData.push(data);
+}
